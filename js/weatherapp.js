cd113cdf5989652aab7b10a3896f0d23087d3f31
Brandon Haupt <haupt.brandon@gmail.com>
2022-10-21 09:01:55 -0500

    Updated weatherapp.js
    Holy balls I did it!

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -17,53 +17,30 @@ function getLocation(location){
     //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
 
     return $.ajax(url).then((data) => {
+        const locationURL = `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=${apiKey}`
+        $.ajax(locationURL)
+        .then((data) => {
+            // console.log(data)
+    
+            const f = Math.floor(1.8*(data.main.feels_like - 273)+32)
+            const temp = Math.floor(1.8*(data.main.temp - 273)+32)
+    
+            // Updating the HTML to display the grabbed info
+            const $main = $(`main`)
+            $main.empty()
         
-        // $main.empty()
-        console.log(data)
-
+            const div = $('<div>')
         
-      
+            div.html(`<p>Weather For: ${data.name}</p>
+            <p>Temperature: ${temp}</p>
+            <p>Feels like: ${f}</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            `)
+        
+            $main.append(div) 
+        })
     })
 }
-
-
-function getWeatherInfo(info){
-    getLocation()
-    
-    console.log(info)
-
-    const locationURL = `https://api.openweathermap.org/data/2.5/weather?lat=38.7802&lon=-90.6228&appid=${apiKey}`
-
-   
-
-    //Example after location gotten
-    //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
-
-    $.ajax(locationURL)
-    .then((data) => {
-        console.log(data)
-
-        const f = Math.floor(1.8*(data.main.feels_like - 273)+32)
-        const temp = Math.floor(1.8*(data.main.temp - 273)+32)
-
-        // Updating the HTML to display the grabbed info
-        const $main = $(`main`)
-        $main.empty()
-    
-        const div = $('<div>')
-    
-        div.html(`<p>Weather For: ${data.name}</p>
-        <p>Temperature: ${temp}</p>
-        <p>Feels like: ${f}</p>
-        <p>Weather: ${data.weather[0].description}</p>
-        `)
-    
-        $main.append(div) 
-    })
-}
-
-
-
 
 // grabbing the on click event
 $('input[type=submit]').on("click", (event) => {
@@ -74,4 +51,4 @@ $('input[type=submit]').on("click", (event) => {
     getLocation(inputText)
 })
 
-getLocation("63376").then(getWeatherInfo())
+getLocation("63376")
